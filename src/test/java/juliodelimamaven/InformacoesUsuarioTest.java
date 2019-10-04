34da36b422ee485685804ab0f3d09231038a1a0a
Jonathan Cavalcanti <qajonathanpoa@gmail.com>
2019-10-04 19:55:41 -0300

    Realizando ajustes refatoracao de codigo

--- src/test/java/juliodelimamaven/InformacoesUsuarioTest.java
+++ src/test/java/juliodelimamaven/InformacoesUsuarioTest.java
@@ -26,51 +26,52 @@ import suporte.Screenshot;
 @DataLoader(filePaths = {"testInformacoesUsuario.csv"})
 
 //@RunWith (DataDrivenTestRunner)
-//A notação "@RunWith",serve para utilizar a classe e biblioteca de testes do Easytest//
+//A notaï¿½ï¿½o "@RunWith",serve para utilizar a classe e biblioteca de testes do Easytest//
 //@DataLoader (filePaths = "testAdicionarInformacaoUsuario.csv" )
-//Utilizando o filepaths que serve para dizer os arquivos que quero utilizar para guardar as informações dos meus testes
-//O interessante é colocar o nome do arquivo como sendo o nome da classe para facilitar a localização dos arquivos de testes//
-//Após informar ao DataLoader o nome e tipo do arquivo que criarei para guardar os dados dos meus testes devo informar
-//o diretório que esse arquivo será armazenado
-//Caso não tenha criado, devo criar a pasta "resources" no seguinte diretório /src/test/resources
-//E dentro do diretório "resources" eu crio o arquivo que será o nomedaclasse.csv
+//Utilizando o filepaths que serve para dizer os arquivos que quero utilizar para guardar as informaï¿½ï¿½es dos meus testes
+//O interessante ï¿½ colocar o nome do arquivo como sendo o nome da classe para facilitar a localizaï¿½ï¿½o dos arquivos de testes//
+//Apï¿½s informar ao DataLoader o nome e tipo do arquivo que criarei para guardar os dados dos meus testes devo informar
+//o diretï¿½rio que esse arquivo serï¿½ armazenado
+//Caso nï¿½o tenha criado, devo criar a pasta "resources" no seguinte diretï¿½rio /src/test/resources
+//E dentro do diretï¿½rio "resources" eu crio o arquivo que serï¿½ o nomedaclasse.csv
 
 public class InformacoesUsuarioTest {
 
 		
-	//Variável navegador criada para ser chamada em vários métodos 
+	//Variï¿½vel navegador criada para ser chamada em vï¿½rios mï¿½todos 
 	private WebDriver navegador;
 	
-	//Utilizando a notação Rule, para que seja possível utilizar o método getMethodName.
-	//Com isso o screenshot será criado com o nome do método que está sendo testado
+	//Utilizando a notaï¿½ï¿½o Rule, para que seja possï¿½vel utilizar o mï¿½todo getMethodName.
+	//Com isso o screenshot serï¿½ criado com o nome do mï¿½todo que estï¿½ sendo testado
 	@Rule
 	public TestName test = new TestName();
 	
 	
 	@Before
 	public void setUP() {
+		
 		navegador = NavegadorFirefox.createFirefox();
 		
-		//Clicar no botão que possui o texto "Signin"
+		//Clicar no botï¿½o que possui o texto "Signin"
 		navegador.findElement(By.linkText("Sign in")).click();
 				
-		//Identificando o campo "login" que está dentro do formulário de id"signinbox"//
+		//Identificando o campo "login" que estï¿½ dentro do formulï¿½rio de id"signinbox"//
 		WebElement formularioSigninbox = navegador.findElement(By.id("signinbox"));
 		
-		//Digitar no campo com name"login" que está dentro do formulário de id"signinbox" o texto "julio0001"//
+		//Digitar no campo com name"login" que estï¿½ dentro do formulï¿½rio de id"signinbox" o texto "julio0001"//
 		formularioSigninbox.findElement(By.name("login")).sendKeys("julio0001");
 			
-		//Digitar no campo com name"login" que está dentro do formulário de id"signinbox" o texto "123456"
+		//Digitar no campo com name"login" que estï¿½ dentro do formulï¿½rio de id"signinbox" o texto "123456"
 		formularioSigninbox.findElement(By.name("password")).sendKeys("123456");
 		
-		//Clicar no botão com o texto "Sign in"
+		//Clicar no botï¿½o com o texto "Sign in"
 		navegador.findElement(By.linkText("SIGN IN")).click();
 				
-		//Validar que dentro do elemento com class "me" está 
+		//Validar que dentro do elemento com class "me" estï¿½ 
 		//o texto "HI, Julio"
 		WebElement me = navegador.findElement(By.className("me"));
 				
-		//o comando pega o que está entre a "Tag", no caso abaixo dentro da tag "a"//
+		//o comando pega o que estï¿½ entre a "Tag", no caso abaixo dentro da tag "a"//
 		String textonoElementome = me.getText();
 		Assert.assertEquals("Hi, Julio", textonoElementome);
 				
@@ -80,26 +81,26 @@ public class InformacoesUsuarioTest {
 		//CLicar em um link que possui o texto "MORE DATA ABOUT YOU"
 		navegador.findElement(By.linkText("MORE DATA ABOUT YOU")).click();
 		
-	}//Método padrão para todos os testes
+	}//Mï¿½todo padrï¿½o para todos os testes
 	
 	@Test
-	//O name do param deve ser exatamente o nome da coluna que está no arquivo, já o nome da string pode ser
+	//O name do param deve ser exatamente o nome da coluna que estï¿½ no arquivo, jï¿½ o nome da string pode ser
 	//o que eu achar melhor
 	public void testInformacoesUsuario(@Param(name="tipo1")String tipo, @Param (name="contato1") String contato, @Param (name="mensagem2") String mensagemEsperada){
 	
 				
-		//Clicar no botão através do id"moredata" que foi o primeiro elemento anterior 
+		//Clicar no botï¿½o atravï¿½s do id"moredata" que foi o primeiro elemento anterior 
 		WebElement divMoreData = navegador.findElement(By.id("moredata"));
 		
-		//Clicado no botão "+ADD MORE DATA" através do cssSelector do elemento
+		//Clicado no botï¿½o "+ADD MORE DATA" atravï¿½s do cssSelector do elemento
 		divMoreData.findElement(By.cssSelector("button[data-target]")).click();
 		
-		//Identificar o formulário anterior dentro da div antecessora de id"addmoredata" 
+		//Identificar o formulï¿½rio anterior dentro da div antecessora de id"addmoredata" 
 		WebElement popupAddmoredata = navegador.findElement(By.id("addmoredata"));
 		
-		//No combobox de name "type" escolher a opção "Phone" no Selenium quando é questão de SELECT
-		// podemos selecionar pelo índice (1 ou 2), pelo "value" ou pelo "texto". nesse caso
-		//Será utilizado o texto que é o que mais se assemelha ao usuário.
+		//No combobox de name "type" escolher a opï¿½ï¿½o "Phone" no Selenium quando ï¿½ questï¿½o de SELECT
+		// podemos selecionar pelo ï¿½ndice (1 ou 2), pelo "value" ou pelo "texto". nesse caso
+		//Serï¿½ utilizado o texto que ï¿½ o que mais se assemelha ao usuï¿½rio.
 		
 		WebElement campoType = popupAddmoredata.findElement(By.name("type"));
 		new Select(campoType).selectByVisibleText(tipo);
@@ -107,16 +108,16 @@ public class InformacoesUsuarioTest {
 		//Colocar no campo de select.by name "contact" telefone "51983136802"
 		popupAddmoredata.findElement(By.name("contact")).sendKeys(contato);
 				
-		//No link de text save "SAVE" que está na popup (utilizar o linktext) que está dentro
+		//No link de text save "SAVE" que estï¿½ na popup (utilizar o linktext) que estï¿½ dentro
 		//do id="addmoredata"
 		popupAddmoredata.findElement(By.linkText("SAVE")).click();
 				
-		//Na mensagem de id "toast-container" validar que o texto é "Your contact has been added!"
+		//Na mensagem de id "toast-container" validar que o texto ï¿½ "Your contact has been added!"
 		WebElement mensagemPop = navegador.findElement(By.id("toast-container"));
 		String mensagem = mensagemPop.getText();
 		Assert.assertEquals(mensagemEsperada, mensagem);
 		
-	}//Fim do método Execução dos testes 
+	}//Fim do mï¿½todo Execuï¿½ï¿½o dos testes 
 	
 	//@Test
 	
@@ -136,17 +137,17 @@ public class InformacoesUsuarioTest {
 		String mensagem = mensagemPop.getText();
 		Assert.assertEquals("Rest in peace, dear phone!", mensagem);
 		
-		//Utilizando a classe screenshot.tirar e método generator//
-		//Importante destacar que com a notação @Rule foi possível definir que a imagem
-		//Será criada com o nome do método
+		//Utilizando a classe screenshot.tirar e mï¿½todo generator//
+		//Importante destacar que com a notaï¿½ï¿½o @Rule foi possï¿½vel definir que a imagem
+		//Serï¿½ criada com o nome do mï¿½todo
 				
 		String screenshotArquivo = "\\Users\\Jonathan\\Desktop\\testeSelenium" + Generator.dataHoraParaArquivo() + test.getMethodName() + ".png";
 		Screenshot.tirar(navegador, screenshotArquivo);
 		
-		//Aguardar até 10 segundos para que a jenela desapareça (utlizando espera explícita)
-		//WebDriverWait é a espera explícita, onde eu digo quanto tempo o navegador vai aguardar
+		//Aguardar atï¿½ 10 segundos para que a jenela desapareï¿½a (utlizando espera explï¿½cita)
+		//WebDriverWait ï¿½ a espera explï¿½cita, onde eu digo quanto tempo o navegador vai aguardar
 		//o desaparecimento do elemento
-		//O stalenessOf é dizer para script aguardar até o elemento desaparecer do DOM
+		//O stalenessOf ï¿½ dizer para script aguardar atï¿½ o elemento desaparecer do DOM
 		WebDriverWait aguardar = new WebDriverWait(navegador, 10);
 		aguardar.until(ExpectedConditions.stalenessOf(mensagemPop));
 		
@@ -154,13 +155,13 @@ public class InformacoesUsuarioTest {
 		navegador.findElement(By.linkText("Logout")).click();		
 
 		
-	}//Fim do segundo método de testes ------------------------------------------------------------------
+	}//Fim do segundo mï¿½todo de testes ------------------------------------------------------------------
 	
 @After
 public void tearDown() {
 	navegador.close();
 	
-}//Fim do método utilizando o @After
+}//Fim do mï¿½todo utilizando o @After
 
 }
 
